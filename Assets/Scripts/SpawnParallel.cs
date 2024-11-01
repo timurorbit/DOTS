a5af5d8db15af07418fb9695cf5d47d40b7258c0
Timur <t.shamilov.hollow@gmail.com>
2024-11-01 16:52:45 +0100

    Parallel movement with job system
    FPS boosted from 30 to 40

--- Assets/Scripts/SpawnParallel.cs
+++ Assets/Scripts/SpawnParallel.cs
@@ -1,29 +1,56 @@
-﻿using UnityEngine;
+﻿using System;
+using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Jobs;
+using Random = UnityEngine.Random;
 
 public class SpawnParallel : MonoBehaviour
 {
     public GameObject sheepPrefab;
-    private GameObject[] allSheep;
+    private Transform[] allSheep;
     
     const int numSheep = 15000;
-    
+
+    private MoveJob moveJob;
+    private JobHandle moveHandle;
+    private TransformAccessArray transforms;
+    struct MoveJob : IJobParallelForTransform
+    {
+        public void Execute(int index, TransformAccess transform)
+        {
+            transform.position += 0.1f * (transform.rotation * new Vector3(0, 0, 1));
+            if (transform.position.z > 50)
+            {
+                transform.position = new Vector3(transform.position.x,0,-50);
+            }
+        }
+    }
+
     void Start()
     {
-        allSheep = new GameObject[numSheep];
+        allSheep = new Transform[numSheep];
         for (int i = 0; i < numSheep; i++)
         {
             Vector3 pos = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50)); 
-            allSheep[i] = Instantiate(sheepPrefab, pos, Quaternion.identity);
+            GameObject sheep = Instantiate(sheepPrefab, pos, Quaternion.identity);
+            allSheep[i] = sheep.transform;
         }
+        transforms = new TransformAccessArray(allSheep);
     }
 
     private void Update()
     {
-        for (int i = 0; i < allSheep.Length; i++)
-        {
-            allSheep[i].transform.Translate(0,0,0.1f);
-            if (allSheep[i].transform.position.z > 50)
-                allSheep[i].transform.position = new Vector3(allSheep[i].transform.position.x, 0, -50);
-        } 
+        moveJob = new MoveJob{};
+        moveHandle = moveJob.Schedule(transforms);
+    }
+
+    private void LateUpdate()
+    {
+        moveHandle.Complete();
+    }
+
+    private void OnDestroy()
+    {
+        transforms.Dispose();
     }
 }
